e2303d0626b0100bea98c7cff0b5223e52f85fde
Abu Almagd <abdulrahmanabualmagd@gmail.com>
2024-07-29 22:32:28 +0300

    Update

--- front/src/components/privateRoute/privateRoutes.jsx
+++ front/src/components/privateRoute/privateRoutes.jsx
@@ -6,8 +6,14 @@
 import { useContext } from "react";
 import { AuthContext } from "contexts";
 import { LoginPage } from "pages";
+import PropTypes from "prop-types";
 
 export default function PrivateRoute({ children }) {
     const { authToken } = useContext(AuthContext);
     return authToken ? children : <LoginPage />;
 }
+
+// children proptype definition
+PrivateRoute.propTypes = {
+    children: PropTypes.node.isRequired,
+};
