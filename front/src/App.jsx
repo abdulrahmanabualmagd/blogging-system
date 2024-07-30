e8669a192be936252de413f4b7f01c911aafed56
Abu Almagd <abdulrahmanabualmagd@gmail.com>
2024-07-30 18:56:06 +0300

    Update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import router from "routes";
 import { AuthProvider } from "contexts";
+import { Provider } from "react-redux";
 import "css";
+import store from "./redux/store";
 
 export default function App() {
     return (
         <>
-            {/* contains the authtoken if found, login/logout methods */}
-            <AuthProvider>
-                {/* contains the routes and the private routes also in routes fille */}
-                <RouterProvider router={router} />
-            </AuthProvider>
+            {/* Provider the store for all children */}
+            <Provider store={store}>
+                {/* contains the authtoken if found, login/logout methods */}
+                <AuthProvider>
+                    {/* contains the routes and the private routes also in routes fille */}
+                    <RouterProvider router={router} />
+                </AuthProvider>
+            </Provider>
         </>
     );
 }
